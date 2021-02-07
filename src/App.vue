33ff10c01a3556cc1c51ad92aac44532a7ee25f5
jramos19-dev <jramos19@southtexascollege.edu>
2021-02-07 01:07:26 -0600

    tailwind auto

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<template>
-  
-  <h1 class ="text-2xl font-bold text-blue-400 font-bold">HELLO There im Jorge !! </h1>
+<template> 
+<h1 class="text-2xl font-bold text-blue-800">Hello There</h1>
 </template>
-
-<script setup>
-</script>
-
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+
 createApp(App).mount('#app')
-
-// we fixed the problems with the warning for the @css
-
