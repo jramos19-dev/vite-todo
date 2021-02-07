3d624b725a588356147064bd1cc941f3217396ee
jramos19-dev <jramos19@southtexascollege.edu>
2021-02-07 00:54:28 -0600

    "changed some setings for the extensions"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   
-  <h1 class ="text-2xl font-bold">HELLO There im Jorge </h1>
+  <h1 class ="text-2xl font-bold text-blue-400 font-bold">HELLO There im Jorge !! </h1>
 </template>
 
 <script setup>
